0e1a6aa87643db3445a710c19f29c6dcee152f42
Gergely Kovacs <gergelyk89@gmail.com>
2019-01-16 19:05:12 +0100

    update config to json file

--- server/models/user.js
+++ server/models/user.js
@@ -39,7 +39,7 @@ UserSchema.methods.generateAuthToken = function () {
   var token = jwt.sign({
       _id: user._id.toHexString(),
       access: access
-  }, 'abc123').toString();
+  }, process.env.JWT_SECRET).toString();
 
   user.tokens = user.tokens.concat({
       access: access,
@@ -97,7 +97,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-      decoded = jwt.verify(token, 'abc123');
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject();
     }
